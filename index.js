4f6d405d7566a42638155575691bcb3c226bdc05
gualves <gustavo.allvess@icloud.com>
2024-06-20 14:46:24 -0300

    add new partials and configuration MYSQL/Sequelize in project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bodyParser = require('body-parser')
+const urlencoded = require('body-parser/lib/types/urlencoded')
 const express = require('express')
 const app = express()
 
@@ -5,8 +7,23 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//Config. bodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Rotas
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/perguntar', (req, res) => {
+  res.render('perguntar')
+})
+
+app.post('/salvarperguntas', (req, res) => {
+  const titulo = req.body.titulo
+  const descricao = req.body.descricao
+  res.send('form recebido titulo: ' + titulo + ' ' + ' descricao: ' + descricao)
 })
 
 app.listen(8080, () => {
